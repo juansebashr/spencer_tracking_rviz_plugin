b4997394333a3f34a56072c086c10bc800b4b334
Sebastian Hernandez <jushernandezre@unal.edu.co>
2023-11-13 02:50:40 +0000

    Fix clang

--- src/additional_topic_subscriber.h
+++ src/additional_topic_subscriber.h
@@ -1,33 +1,33 @@
 /*
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2013-2015, Timm Linder, Social Robotics Lab, University of Freiburg
-*  Copyright (c) 2012, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-*
-*  * Redistributions of source code must retain the above copyright notice, this
-*    list of conditions and the following disclaimer.
-*  * Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*  * Neither the name of the copyright holder nor the names of its contributors
-*    may be used to endorse or promote products derived from this software
-*    without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013-2015, Timm Linder, Social Robotics Lab, University of Freiburg
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef ADDITIONAL_TOPIC_SUBSCRIBER_H
 #define ADDITIONAL_TOPIC_SUBSCRIBER_H
 
@@ -36,16 +36,16 @@
 #include <message_filters/subscriber.h>
 
 #if ROS_VERSION_MINIMUM(1, 14, 0)
-  #include <tf2_ros/message_filter.h>
+#include <tf2_ros/message_filter.h>
 #else
-  #include <tf/message_filter.h>
+#include <tf/message_filter.h>
 #endif
 
+#include <OGRE/OgreSceneManager.h>
+#include <OGRE/OgreSceneNode.h>
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
 #include <iostream>
-#include <OGRE/OgreSceneManager.h>
-#include <OGRE/OgreSceneNode.h>
 #endif
 
 #include <rviz/display.h>
@@ -53,32 +53,31 @@
 #include <rviz/frame_manager.h>
 #include <rviz/properties/ros_topic_property.h>
 
-
 using namespace boost;
 
-namespace rviz
-{
+namespace rviz {
 
 /** @brief Helper superclass for AdditionalTopicSubscriber, needed because
  * Qt's moc and c++ templates don't work nicely together.  Not
  * intended to be used directly. */
-class _AdditionalTopicSubscriber: QObject
+class _AdditionalTopicSubscriber : QObject
 {
-Q_OBJECT
-public:
+    Q_OBJECT
+   public:
     void initialize(Display* display, FrameManager* frameManager)
     {
         QObject::connect(display, SIGNAL(changed()), this, SLOT(displayEnableChanged()));
         QObject::connect(frameManager, SIGNAL(fixedFrameChanged()), this, SLOT(fixedFrameChanged()));
-        additional_topic_property_ = new RosTopicProperty("Additional topic", "", "", "", display, SLOT(updateTopic()), this);
+        additional_topic_property_ =
+            new RosTopicProperty("Additional topic", "", "", "", display, SLOT(updateTopic()), this);
     }
 
-protected Q_SLOTS:
+   protected Q_SLOTS:
     virtual void updateTopic() = 0;
     virtual void displayEnableChanged() = 0;
     virtual void fixedFrameChanged() = 0;
 
-protected:
+   protected:
     RosTopicProperty* additional_topic_property_;
 };
 
@@ -88,17 +87,25 @@ protected:
  * types.  It has a tf::MessageFilter to filter incoming messages, and
  * it handles subscribing and unsubscribing when the display is
  * enabled or disabled.  It also has an Ogre::SceneNode which  */
-template<class MessageType>
-class AdditionalTopicSubscriber: public _AdditionalTopicSubscriber
+template <class MessageType>
+class AdditionalTopicSubscriber : public _AdditionalTopicSubscriber
 {
-// No Q_OBJECT macro here, moc does not support Q_OBJECT in a templated class.
-public:
+    // No Q_OBJECT macro here, moc does not support Q_OBJECT in a templated class.
+   public:
     /** @brief Convenience typedef so subclasses don't have to use
      * the long templated class name to refer to their super class. */
     typedef AdditionalTopicSubscriber<MessageType> ATSClass;
 
-    AdditionalTopicSubscriber(const QString& propertyName, Display* display, DisplayContext* context, ros::NodeHandle& update_nh, const boost::function<void (boost::shared_ptr<const MessageType>)>& messageCallback)
-            : tf_filter(NULL), m_messagesReceived(0), m_display(display), m_context(context), m_updateNodeHandle(update_nh), m_enabled(false), m_messageCallback(messageCallback)
+    AdditionalTopicSubscriber(const QString& propertyName, Display* display, DisplayContext* context,
+                              ros::NodeHandle& update_nh,
+                              const boost::function<void(boost::shared_ptr<const MessageType>)>& messageCallback)
+        : tf_filter(NULL)
+        , m_messagesReceived(0)
+        , m_display(display)
+        , m_context(context)
+        , m_updateNodeHandle(update_nh)
+        , m_enabled(false)
+        , m_messageCallback(messageCallback)
     {
         _AdditionalTopicSubscriber::initialize(display, context->getFrameManager());
 
@@ -107,11 +114,12 @@ public:
         additional_topic_property_->setMessageType(message_type);
         additional_topic_property_->setDescription(message_type + " topic to subscribe to.");
 
-        #if ROS_VERSION_MINIMUM(1, 14, 0)
-            tf_filter = new tf2_ros::MessageFilter<MessageType>(*m_context->getTF2BufferPtr(), "map", 10, m_updateNodeHandle);
-        #else
-            tf_filter = new tf::MessageFilter<MessageType>(*m_context->getTFClient(), "map", 10, m_updateNodeHandle);
-        #endif
+#if ROS_VERSION_MINIMUM(1, 14, 0)
+        tf_filter =
+            new tf2_ros::MessageFilter<MessageType>(*m_context->getTF2BufferPtr(), "map", 10, m_updateNodeHandle);
+#else
+        tf_filter = new tf::MessageFilter<MessageType>(*m_context->getTFClient(), "map", 10, m_updateNodeHandle);
+#endif
 
         tf_filter->connectInput(m_subscriber);
         tf_filter->registerCallback(boost::bind(&AdditionalTopicSubscriber<MessageType>::incomingMessage, this, _1));
@@ -140,10 +148,11 @@ public:
         if (enabled) subscribe();
     }
 
-protected:
+   protected:
     virtual void updateTopic()
     {
-        ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber", "AdditionalTopicSubscriber: Topic was changed to " << additional_topic_property_->getTopicStd());
+        ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber", "AdditionalTopicSubscriber: Topic was changed to "
+                                                                << additional_topic_property_->getTopicStd());
         unsubscribe();
         reset();
         subscribe();
@@ -152,61 +161,67 @@ protected:
 
     virtual void displayEnableChanged()
     {
-        ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber", "AdditionalTopicSubscriber: Display enabled = " << m_display->getBool());
+        ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber",
+                               "AdditionalTopicSubscriber: Display enabled = " << m_display->getBool());
         setEnabled(m_display->getBool());
     }
 
     virtual void fixedFrameChanged()
     {
-        ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber", "AdditionalTopicSubscriber: Fixed frame has changed for topic " << additional_topic_property_->getTopicStd());
+        ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber",
+                               "AdditionalTopicSubscriber: Fixed frame has changed for topic "
+                                   << additional_topic_property_->getTopicStd());
         tf_filter->setTargetFrame(m_context->getFixedFrame().toStdString());
         reset();
     }
 
     virtual void subscribe()
     {
-        if (!m_display->isEnabled()) {
+        if (!m_display->isEnabled())
+        {
             return;
         }
 
-        try {
-            ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber", "AdditionalTopicSubscriber: Subscribing to topic " << additional_topic_property_->getTopicStd());
+        try
+        {
+            ROS_DEBUG_STREAM_NAMED("AdditionalTopicSubscriber", "AdditionalTopicSubscriber: Subscribing to topic "
+                                                                    << additional_topic_property_->getTopicStd());
             m_subscriber.subscribe(m_updateNodeHandle, additional_topic_property_->getTopicStd(), 10);
             m_display->setStatus(StatusProperty::Ok, additional_topic_property_->getName(), "OK");
-        }
-        catch (ros::Exception& e) {
-            m_display->setStatus(StatusProperty::Error, additional_topic_property_->getName(), QString("Error subscribing: ") + e.what());
+        } catch (ros::Exception& e)
+        {
+            m_display->setStatus(StatusProperty::Error, additional_topic_property_->getName(),
+                                 QString("Error subscribing: ") + e.what());
         }
     }
 
-    virtual void unsubscribe()
-    {
-        m_subscriber.unsubscribe();
-    }
+    virtual void unsubscribe() { m_subscriber.unsubscribe(); }
 
     /** @brief Incoming message callback.  Checks if the message pointer
      * is valid, increments m_messagesReceived, then calls
      * processMessage(). */
     void incomingMessage(const typename MessageType::ConstPtr& msg)
     {
-        if (!msg) {
+        if (!msg)
+        {
             return;
         }
 
         ++m_messagesReceived;
-        m_display->setStatus(StatusProperty::Ok, additional_topic_property_->getName(), QString::number(m_messagesReceived) + " messages received");
+        m_display->setStatus(StatusProperty::Ok, additional_topic_property_->getName(),
+                             QString::number(m_messagesReceived) + " messages received");
 
         // Callback for further processing
         m_messageCallback(msg);
     }
 
-    #if ROS_VERSION_MINIMUM(1, 14, 0)
-        tf2_ros::MessageFilter<MessageType>* tf_filter;
-    #else
-        tf::MessageFilter<MessageType>* tf_filter;
-    #endif
+#if ROS_VERSION_MINIMUM(1, 14, 0)
+    tf2_ros::MessageFilter<MessageType>* tf_filter;
+#else
+    tf::MessageFilter<MessageType>* tf_filter;
+#endif
 
-private:
+   private:
     std::string m_topic;
     bool m_enabled;
     Display* m_display;
@@ -215,9 +230,9 @@ private:
     message_filters::Subscriber<MessageType> m_subscriber;
     uint32_t m_messagesReceived;
 
-    const boost::function<void (boost::shared_ptr<const MessageType>)> m_messageCallback;
+    const boost::function<void(boost::shared_ptr<const MessageType>)> m_messageCallback;
 };
 
-} // end namespace rviz
+}  // end namespace rviz
 
-#endif // ADDITIONAL_TOPIC_SUBSCRIBER_H
+#endif  // ADDITIONAL_TOPIC_SUBSCRIBER_H
--- src/detected_persons_display.h
+++ src/detected_persons_display.h
@@ -1,114 +1,111 @@
 /*
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2013-2015, Timm Linder, Social Robotics Lab, University of Freiburg
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-*
-*  * Redistributions of source code must retain the above copyright notice, this
-*    list of conditions and the following disclaimer.
-*  * Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*  * Neither the name of the copyright holder nor the names of its contributors
-*    may be used to endorse or promote products derived from this software
-*    without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013-2015, Timm Linder, Social Robotics Lab, University of Freiburg
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef DETECTED_PERSONS_DISPLAY_H
 #define DETECTED_PERSONS_DISPLAY_H
 
 #ifndef Q_MOC_RUN
-#include <map>
-#include <boost/circular_buffer.hpp>
 #include <spencer_tracking_msgs/DetectedPersons.h>
+#include <boost/circular_buffer.hpp>
+#include <map>
 #include "person_display_common.h"
 #endif
 
-namespace spencer_tracking_rviz_plugin
+namespace spencer_tracking_rviz_plugin {
+/// The visual of a tracked person.
+struct DetectedPersonVisual
 {
-    /// The visual of a tracked person.
-    struct DetectedPersonVisual
-    {
-        boost::shared_ptr<Ogre::SceneNode> sceneNode;
+    boost::shared_ptr<Ogre::SceneNode> sceneNode;
 
-        boost::shared_ptr<PersonVisual> personVisual;
-        boost::shared_ptr<TextNode> detectionIdText, confidenceText, modalityText;
-        boost::shared_ptr<rviz::Arrow> orientationArrow;
-        boost::shared_ptr<CovarianceVisual> covarianceVisual;
+    boost::shared_ptr<PersonVisual> personVisual;
+    boost::shared_ptr<TextNode> detectionIdText, confidenceText, modalityText;
+    boost::shared_ptr<rviz::Arrow> orientationArrow;
+    boost::shared_ptr<CovarianceVisual> covarianceVisual;
 
-        float confidence;
-        bool hasValidOrientation;
-        unsigned int detectionId;
-    };
+    float confidence;
+    bool hasValidOrientation;
+    unsigned int detectionId;
+};
 
-    // The DetectedPersonsDisplay class itself just implements a circular buffer,
-    // editable parameters, and Display subclass machinery.
-    class DetectedPersonsDisplay: public PersonDisplayCommon<spencer_tracking_msgs::DetectedPersons>
-    {
+// The DetectedPersonsDisplay class itself just implements a circular buffer,
+// editable parameters, and Display subclass machinery.
+class DetectedPersonsDisplay : public PersonDisplayCommon<spencer_tracking_msgs::DetectedPersons>
+{
     Q_OBJECT
-    public:
-        // Constructor.  pluginlib::ClassLoader creates instances by calling
-        // the default constructor, so make sure you have one.
-        DetectedPersonsDisplay() {};
-        virtual ~DetectedPersonsDisplay();
+   public:
+    // Constructor.  pluginlib::ClassLoader creates instances by calling
+    // the default constructor, so make sure you have one.
+    DetectedPersonsDisplay(){};
+    virtual ~DetectedPersonsDisplay();
 
-        // Overrides of protected virtual functions from Display.  As much
-        // as possible, when Displays are not enabled, they should not be
-        // subscribed to incoming data and should not show anything in the
-        // 3D view.  These functions are where these connections are made
-        // and broken.
-        
-        virtual void onInitialize();
+    // Overrides of protected virtual functions from Display.  As much
+    // as possible, when Displays are not enabled, they should not be
+    // subscribed to incoming data and should not show anything in the
+    // 3D view.  These functions are where these connections are made
+    // and broken.
 
-    protected:
-        // A helper to clear this display back to the initial state.
-        virtual void reset();
+    virtual void onInitialize();
 
-        // Must be implemented by derived classes because MOC doesn't work in templates
-        virtual rviz::DisplayContext* getContext() {
-            return context_;
-        }
+   protected:
+    // A helper to clear this display back to the initial state.
+    virtual void reset();
 
-    private Q_SLOTS:
-        void personVisualTypeChanged();
+    // Must be implemented by derived classes because MOC doesn't work in templates
+    virtual rviz::DisplayContext* getContext() { return context_; }
 
-        // Called whenever one of the properties in PersonDisplayCommonProperties has been changed
-        virtual void stylesChanged();
+   private Q_SLOTS:
+    void personVisualTypeChanged();
 
-    private:
-        // Function to handle an incoming ROS message.
-        void processMessage(const spencer_tracking_msgs::DetectedPersons::ConstPtr& msg);
-       
-        // All currently active tracks, with unique track ID as map key
-        vector<boost::shared_ptr<DetectedPersonVisual> > m_previousDetections;
+    // Called whenever one of the properties in PersonDisplayCommonProperties has been changed
+    virtual void stylesChanged();
 
-        // Properties
-        rviz::BoolProperty* m_render_covariances_property;
-        rviz::BoolProperty* m_render_detection_ids_property;
-        rviz::BoolProperty* m_render_confidences_property;
-        rviz::FloatProperty* m_low_confidence_threshold_property;
-        rviz::FloatProperty* m_low_confidence_alpha_property;
-        rviz::BoolProperty* m_render_orientations_property;
-        rviz::BoolProperty* m_render_modality_text_property;
+   private:
+    // Function to handle an incoming ROS message.
+    void processMessage(const spencer_tracking_msgs::DetectedPersons::ConstPtr& msg);
 
-        rviz::FloatProperty* m_text_spacing_property;
-        rviz::FloatProperty* m_covariance_line_width_property;
-    };
+    // All currently active tracks, with unique track ID as map key
+    vector<boost::shared_ptr<DetectedPersonVisual> > m_previousDetections;
 
-} // end namespace spencer_tracking_rviz_plugin
+    // Properties
+    rviz::BoolProperty* m_render_covariances_property;
+    rviz::BoolProperty* m_render_detection_ids_property;
+    rviz::BoolProperty* m_render_confidences_property;
+    rviz::FloatProperty* m_low_confidence_threshold_property;
+    rviz::FloatProperty* m_low_confidence_alpha_property;
+    rviz::BoolProperty* m_render_orientations_property;
+    rviz::BoolProperty* m_render_modality_text_property;
 
-#endif // DETECTED_PERSONS_DISPLAY_H
+    rviz::FloatProperty* m_text_spacing_property;
+    rviz::FloatProperty* m_covariance_line_width_property;
+};
+
+}  // end namespace spencer_tracking_rviz_plugin
+
+#endif  // DETECTED_PERSONS_DISPLAY_H
